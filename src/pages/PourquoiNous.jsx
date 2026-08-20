0096405aa02fbc392316a98355eb1974bb89938c
Alphonsine <goutinsino@gmail.com>
2026-08-20 10:10:33 +0100

    changement de wrap

--- src/pages/PourquoiNous.jsx
+++ src/pages/PourquoiNous.jsx
@@ -125,7 +125,7 @@ function PourquoiNous(){
                       <div className="max-w-7xl m-auto">
                           <div className="grid grid-cols-1 md:grid-cols-2 space-x-40 space-y-20">
                               <div className="space-y-3">
-                                  <h3 className="text-2xl md:text-4xl font-extrabold">Vous avez un projet <br /> de construction ?</h3>
+                                  <h3 className="text-2xl md:text-4xl font-extrabold text-nowrap">Vous avez un projet <br /> de construction ?</h3>
                                   <p className="text-gray-400 text-sm">Parlons de votre projet et construisons ensemble une solution adaptée à vos besoins.</p>
                               </div>
 
